80fc30dcc5c4da99de516d22911c30c0398a2fd4
Michael Fenwick <mickyfen17@aol.com>
2017-01-07 09:21:34 -0700

    Fix paddle-test file

--- test/paddle-test.js
+++ test/paddle-test.js
@@ -4,13 +4,23 @@ var assert = chai.assert;
 var Paddle = require("../lib/paddle");
 
 describe("Paddle", function() {
+  var paddle = new Paddle({});
 
   it("should be a function", function() {
     assert.isFunction(Paddle);
   });
+
   it("movePaddle should be a function", function() {
-    var paddle = new Paddle();
     assert.isFunction(paddle.movePaddle);
   });
-  
+
+  // it("paddle should move right and moveRight should be true", function() {
+  //   paddle.movePaddle();
+  //   assert.equal(paddle.moveRight, true);
+  // });
+  //
+  // it("paddle should move left and moveLeft should be true", function() {
+  //   paddle.movePaddle();
+  //   assert.equal(paddle.moveLeft, true);
+  // });
 });
